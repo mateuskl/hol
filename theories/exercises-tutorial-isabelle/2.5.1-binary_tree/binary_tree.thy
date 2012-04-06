68775c6971652874878a58008f31eccb3918a98f
Mateus Krepsky Ludwich <mkludwich@gmail.com>
2012-04-06 00:39:09 -0300

    Starting exercise 2.5.1 (tutorial book).

---
+++ theories/exercises-tutorial-isabelle/2.5.1-binary_tree/binary_tree.thy
@@ -0,0 +1,44 @@
+(* 
+Exercise 2.5.1
+
+Define the datatype of binary trees:
+datatype 'a tree = Tip | Node "'a tree" 'a "'a tree"
+
+Define a function mirror that mirrors a binary tree by swapping subtrees
+recursively. 
+
+Prove
+lemma mirror_mirror: "mirror(mirror t) = t"
+
+Define a function flatten that flattens a tree into a list by traversing it in
+infix order. 
+Prove
+lemma "flatten(mirror t) = rev(flatten t)"
+
+*)
+theory ToyBinaryThree
+imports Main
+
+begin
+(* Datatype *)
+datatype 'a tree = 
+  Tip | 
+  Node "'a tree" 'a "'a tree"
+
+
+(* Function definitions *)
+primrec mirror :: "'a tree \<Rightarrow> 'a tree"
+where
+  "mirror Tip = Tip " |
+  "mirror (Node left v right) = Node (mirror (right)) v (mirror (left))"
+
+
+(* Some random tests *)
+(* value "Node (Node (Node Tip 2 Tip) 1 (Node Tip 3 Tip)) 0 (Node (Node Tip 5 Tip) 4 (Node Tip 6 Tip)" *)
+value "Tip"
+
+
+(* Proofs *)
+
+end
+
